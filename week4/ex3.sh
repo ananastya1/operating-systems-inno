793874548fe25412aeb130852eada3e44f06cf31
Anastasia <nastya10_tv@mail.ru>
2022-09-27 11:09:50 +0300

    remake tree view for ex3 week4

--- week4/ex3.sh
+++ week4/ex3.sh
@@ -1,13 +1,15 @@
-gcc ~/week4/ex3.c &
-./a.out 3
+gcc ~/week4/ex3.c 
+./a.out 3 &
 echo "for n = 3: "
-for i in 1 2 3 4 5
+for i in 1 2 3
 do
-   pstree
+   pstree $$
+   sleep 5
 done
-./a.out 5
+./a.out 5 &
 echo "for n = 5: "
 for i in 1 2 3 4 5
 do
-   pstree
+   pstree $$
+   sleep 5
 done
