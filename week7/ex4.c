25af7061023bcf38a3b18fa622e051ae83314d7c
Anastasia <nastya10_tv@mail.ru>
2022-10-25 18:41:45 +0300

    remake ex4

--- week7/ex4.c
+++ week7/ex4.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-void *realloc_(void *ptr, size_t new, size_t old) {
+void *realloc_(void *ptr, size_t new) {
     void *arr = malloc(new); 
+    size_t old = 0;
+    if (ptr != NULL) {
+        old = malloc_usable_size(ptr);
+    }
     if (new < old) {
         memcpy(arr, ptr, new);
     } else {
