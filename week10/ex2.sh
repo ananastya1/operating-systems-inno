dbd2f4ce5c51604efc7b251973b14d9ce4ac2bc5
Anastasia <nastya10_tv@mail.ru>
2022-11-15 18:22:23 +0300

    change ex2

--- week10/ex2.sh
+++ week10/ex2.sh
@@ -1,5 +1,7 @@
-mkdir ../week01
-echo "Nastya" > ../week01/file.txt
-link ../week01/file.txt _ex2.txt
-find ../week01/file.txt -inum $(stat -c '%i' ../week01/file.txt) > ex2.txt
-find ../week01/file.txt -inum $(stat -c '%i' ../week01/file.txt) -exec rm {} \; >> ex2.txt
+mkdir week01
+cd week01
+echo "Nastya" >> file.txt
+cd ..
+link week01/file.txt _ex2.txt
+find ./ -inum "$(stat -c '%i' week01/file.txt)" > ./ex2.txt
+find ./ -inum "$(stat -c '%i' week01/file.txt)" -exec rm {} \; >> ./ex2.txt
